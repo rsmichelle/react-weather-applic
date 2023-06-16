f471facca603f2b9783f3e5bddf83d2d3a167fa8
rsmichelle <rs.michellej@gmail.com>
2023-06-15 22:27:40 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        Coded by Michelle R.
-        <a href="https://github.com/rsmichelle/react-weather-applic">
-          Open source
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          Coded by Michelle R. {""}
+          <a href="https://github.com/rsmichelle/react-weather-applic">
+            Open source
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
