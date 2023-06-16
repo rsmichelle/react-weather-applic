360943b1faeb722b06bf51430fe776bb42c79e8d
rsmichelle <rs.michellej@gmail.com>
2023-06-16 19:11:23 -0400

    added css to App weather

--- src/Weather.js
+++ src/Weather.js
@@ -10,10 +10,15 @@ export default function Weather() {
               type="search"
               placeholder="Type city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary W-100"
+            />
           </div>
         </div>
       </form>
@@ -22,9 +27,20 @@ export default function Weather() {
         <li>Wednesday 10:36</li>
         <li>Clear</li>
       </ul>
-      <p>
-        <im></im>6 C|F
-      </p>
+      <div className="clearfix">
+        <div className="tempWeather">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="clear"
+            className="float-left"
+          />
+          <span className="temperature">6</span>
+          <span className="unit">
+            <span className="unitCels">C</span>
+            <span className="unitFaren">|F</span>
+          </span>
+        </div>
+      </div>
       <div className="row">
         <div className="col-6">
           <ul>
