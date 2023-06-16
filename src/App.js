b6808cbed1764b5f079aeaff057bd9fbc7bc467f
rsmichelle <rs.michellej@gmail.com>
2023-06-15 21:58:43 -0400

    Added open source

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        Coded by Michelle R.
+        <a href="https://github.com/rsmichelle/react-weather-applic">
+          Open source
+        </a>
+      </footer>
+    </div>
+  );
 }
